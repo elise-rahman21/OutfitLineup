ae09f93c289814c275cac12ce4eaf8b33539f262
Skyla Hendrix <Hen514100@d204.lths.net>
2025-04-29 11:47:16 -0500

    Skyla's changes

---
+++ OutfitLineup/TodaysView.swift
@@ -0,0 +1,18 @@
+//
+//  TodaysView.swift
+//  OutfitLineup
+//
+//  Created by Hendrix, Skyla (514100) on 4/29/25.
+//
+
+import SwiftUI
+
+struct TodaysView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodaysView()
+}
